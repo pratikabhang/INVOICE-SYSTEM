f9207637b57ac26a10f664be6e8b6ac2270f29d6
pratikabhang <pratiksunilabhang@gmail.com>
2024-04-28 15:56:14 +0530

    Update invoice_db.sql

--- invoice_db.sql
+++ invoice_db.sql
@@ -1,8 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2024 at 12:24 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `invoice_db`
 --
-CREATE DATABASE IF NOT EXISTS `invoice_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `invoice_db`;
 
 -- --------------------------------------------------------
 
@@ -10,16 +27,22 @@ USE `invoice_db`;
 -- Table structure for table `invoice`
 --
 
-CREATE TABLE IF NOT EXISTS `invoice` (
-  `SID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `invoice` (
+  `SID` int(11) NOT NULL,
   `INVOICE_NO` int(11) NOT NULL,
   `INVOICE_DATE` date NOT NULL,
   `CNAME` varchar(50) NOT NULL,
   `CADDRESS` varchar(150) NOT NULL,
   `CCITY` varchar(50) NOT NULL,
-  `GRAND_TOTAL` double(10,2) NOT NULL,
-  PRIMARY KEY (`SID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `GRAND_TOTAL` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `invoice`
+--
+
+INSERT INTO `invoice` (`SID`, `INVOICE_NO`, `INVOICE_DATE`, `CNAME`, `CADDRESS`, `CCITY`, `GRAND_TOTAL`) VALUES
+(11, 202401, '2024-04-26', 'Pratik Abhang', 'Shrirampur', '8766854678', 383.00);
 
 -- --------------------------------------------------------
 
@@ -27,15 +50,57 @@ CREATE TABLE IF NOT EXISTS `invoice` (
 -- Table structure for table `invoice_products`
 --
 
-CREATE TABLE IF NOT EXISTS `invoice_products` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `invoice_products` (
+  `ID` int(11) NOT NULL,
   `SID` int(11) NOT NULL,
   `PNAME` varchar(100) NOT NULL,
   `PRICE` double(10,2) NOT NULL,
   `QTY` int(11) NOT NULL,
-  `TOTAL` double(10,2) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `TOTAL` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `invoice_products`
+--
+
+INSERT INTO `invoice_products` (`ID`, `SID`, `PNAME`, `PRICE`, `QTY`, `TOTAL`) VALUES
+(19, 11, 'snack ', 20.00, 6, 120.00),
+(20, 11, 'milk', 84.00, 2, 168.00),
+(21, 11, 'dairy milk ', 10.00, 5, 50.00),
+(22, 11, 'wafers', 15.00, 3, 45.00);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`SID`);
+
+--
+-- Indexes for table `invoice_products`
+--
+ALTER TABLE `invoice_products`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `SID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `invoice_products`
+--
+ALTER TABLE `invoice_products`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
